75b613ebd383f6a6e5df0f0e2772bb1a48ba7e10
Diego Fernández Arenas <dferare1310@g.educaand.es>
2026-02-16 20:18:26 +0100

    Creacion iniciales de 3 generación

--- src/CreadorPokemons.java
+++ src/CreadorPokemons.java
@@ -24,46 +24,61 @@ public class CreadorPokemons {
     /**
      * CreacionPokemon
      */
+    
+    // GEN 1
     Pokémon charmander;
     Pokémon bulbasur;
     Pokémon squirtle;
+    
+    // GEN 3
+     Pokémon treecko;
+     Pokémon torchic;
+     Pokémon mudkip;
 
     public void inicializarPokemons(){
-
-
         /**
          * Lo he hecho asi para que sea muy facil meterlos y se puedan reutilizar
          */
 
         //Gen 1
-        charmander =new Pokémon(1,"Charmander",TipoPokemon.FUEGO,TipoPokemon.FUEGO,Generacion.GEN1,1,100,100);
+        charmander =new Pokémon(1,"Charmander",TipoPokemon.FUEGO,TipoPokemon.FUEGO,Generacion.GEN1,1,39,39);
         charmander.aprenderAtaque(ascuas);
         gen1.add(charmander);
         /// //////////
-        bulbasur =new Pokémon(2,"Bulbasur",TipoPokemon.PLANTA,TipoPokemon.PLANTA,Generacion.GEN1,1,100,100);
+        bulbasur =new Pokémon(2,"Bulbasur",TipoPokemon.PLANTA,TipoPokemon.PLANTA,Generacion.GEN1,1,45,45);
         bulbasur.aprenderAtaque(latigoCepa);
         bulbasur.aprenderAtaque(drenadoras);
         gen1.add(bulbasur);
         /// //////////
-        squirtle = new Pokémon(2,"Squirtle",TipoPokemon.AGUA,TipoPokemon.AGUA,Generacion.GEN1,1,100,100);
+        squirtle = new Pokémon(3,"Squirtle",TipoPokemon.AGUA,TipoPokemon.AGUA,Generacion.GEN1,1,44,44);
         squirtle.aprenderAtaque(burbuja);
         gen1.add(squirtle);
 
         pokemonsPorGeneracion.put(Generacion.GEN1,gen1);
         /// //////////
-        //Gen2
+        //Gen3
+         treecko = new Pokémon(4, "Treecko", TipoPokemon.PLANTA, TipoPokemon.PLANTA, Generacion.GEN3, 1,40,40);
+         treecko.aprenderAtaque(latigoCepa);
+         torchic = new Pokémon(5, "Torchic", TipoPokemon.FUEGO, TipoPokemon.FUEGO, Generacion.GEN3, 1,45,45);
+         torchic.aprenderAtaque(ascuas);
+         mudkip = new Pokémon(6, "Mudkip", TipoPokemon.AGUA, TipoPokemon.AGUA, Generacion.GEN3, 1,50,50);
+         mudkip.aprenderAtaque(burbuja);
     }
 
 
-    public String AtaquesCharmander(){
+     public String AtaquesCharmander(){
         return charmander.mostrarAtaques();
     }
-    public String AtaquesBulbasur(){
+     public String AtaquesBulbasur(){
         return bulbasur.mostrarAtaques();
     }
-    public String AtaquesSquirtle(){
+     public String AtaquesSquirtle(){
         return squirtle.mostrarAtaques();
     }
+    
+     public String AtaquesTorchic(){return torchic.mostrarAtaques();}
+     public String AtaquesTreecko(){return treecko.mostrarAtaques();}
+     public String AtaquesMudkip(){return mudkip.mostrarAtaques();}
 
 
     @Override
